d48a2527e51efd7d0aafdc4a6ed1cb5f3621cfd5
Ines Oliveira e Silva <inesoliveiraesilva@gmail.com>
2020-03-14 16:03:27 +0000

    cleanup

--- cal_fp02_CLion/cal_fp02_CLion/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ineso/OneDrive/Documentos/Ines-Universidade/2 ano/2 semestre/CAL/cal_fp02_CLion/cal_fp02_CLion/lib/googletest-master/googlemock
-# Build directory: C:/Users/ineso/OneDrive/Documentos/Ines-Universidade/2 ano/2 semestre/CAL/cal_fp02_CLion/cal_fp02_CLion/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
