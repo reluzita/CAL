e06e8f38de782d6613364552f97358e8aa9b3b20
Ines Oliveira e Silva <inesoliveiraesilva@gmail.com>
2020-03-28 11:10:21 +0000

    TP5 ex 1 and 2

---
+++ cal_fp05_CLion/cmake-build-debug/CMakeFiles/CAL_FP05.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CAL_FP05.dir/main.cpp.obj"
+  "CMakeFiles/CAL_FP05.dir/Tests/tests.cpp.obj"
+  "CAL_FP05.pdb"
+  "CAL_FP05.exe"
+  "CAL_FP05.exe.manifest"
+  "libCAL_FP05.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CAL_FP05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cal_fp05_CLion/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ineso/OneDrive/Documentos/Ines-Universidade/2 ano/2 semestre/CAL/cal_fp05_CLion/lib/googletest-master/googlemock
+# Build directory: C:/Users/ineso/OneDrive/Documentos/Ines-Universidade/2 ano/2 semestre/CAL/cal_fp05_CLion/cmake-build-debug/lib/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
